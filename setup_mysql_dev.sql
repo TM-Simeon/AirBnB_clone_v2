753dbc02f427c4dea5dc63d757d4b1b66e0af666
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-11-13 22:50:02 +0100

    update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,6 +3,6 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- create a user and grant privileges
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- grant SELECT privileges on performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
