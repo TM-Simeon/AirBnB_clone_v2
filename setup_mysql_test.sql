d7e1230285bcfb8af671f72f89a15010e3c7e47b
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-11-13 22:56:00 +0100

    update

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,9 +2,8 @@
 -- creates a database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- create a user and grant privileges
-GRANT ALL ON hbnb_test_db.*
-TO 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- grant all privileges on hbnb_test_db
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- grant SELECT privileges
-GRANT SELECT ON performance_schema.*
-TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
